8178f49d58789e320ecdc02d2614bf3af670999a
dtroy345 <59236999+dtroy345@users.noreply.github.com>
2026-05-26 22:36:44 -0400

    added in skills and styled it

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,26 @@
+//add a footer
 const grabConnectSection = document.getElementById("connect");
 const addFooter = document.createElement("footer");
 connect.after(addFooter);
 addFooter.append("Footer Placement");
 addFooter.setAttribute("id", "newFooter");
 
-//  Create a variable named today and assign it a new date object
+// add the name and copyright
 const today = new Date();
-// hint: new Date() constructor
-//  Create a variable named thisYear and assign it the current year from your date object. Use a method to do this, don't hardcode "2024" because next year your website will have the wrong year!
 const thisYear = today.getFullYear();
 console.log(thisYear);
-// hint: getFullYear method
-//  Create a variable named footer and assign it the footer element by using "DOM Selection" to select the <footer> element from the DOM
 const footer = document.querySelector("footer");
 console.log("you got the footer");
-// hint: querySelector method or similar
-//  Create a variable named copyright and use it to create a new paragraph (p) element
 const copyright = document.createElement("p");
-// hint: createElement method
-//  Set the inner HTML of your copyright element to display your name and the current year
-// hint: use the copyright variable and the thisYear variable from earlier
 copyright.innerHTML = `Alix ${thisYear} ©`;
-//  Append the copyright element to the footer using "DOM Manipulation"
-// hint: appendChild method or similar
 footer.appendChild(copyright);
-//  STRETCH GOAL: Use unicode to also include the copyright symbol ( © ) in your footer content
-//  Save and refresh your browser
-// You should see the text "Your Name 2024" at the bottom of the page
+
+// add in the skills list
+const skills = ["Javascript", "HTML", "CSS", "Github", "SQL"];
+const skillsSection = document.getElementById("skills");
+const skillsList = document.querySelector("#skills ul");
+for (let i = 0; i < skills.length; i++) {
+	const skill = document.createElement("li");
+	skill.textContent = skills[i];
+	skillsList.appendChild(skill);
+}
